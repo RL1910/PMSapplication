581a0ab7caaa14799f62ccbe78897514ebb0ca74
RL1910 <102343574+RL1910@users.noreply.github.com>
2022-05-12 01:40:02 +0530

    Update BuyerService.java

--- src/main/java/com/cg/services/BuyerService.java
+++ src/main/java/com/cg/services/BuyerService.java
@@ -3,7 +3,7 @@ package com.cg.services;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,54 +12,49 @@ import org.springframework.stereotype.Service;
 
 import com.cg.entities.Buyer;
 import com.cg.entities.Preference;
-import com.cg.entities.Property;
-import com.cg.exception.PersonNotFoundException;
-import com.cg.exception.PropertyNotFoundException;
+import com.cg.exception.PersonNotFountException;
 import com.cg.repositories.BuyerRepository;
-import com.cg.repositories.PropertyRepository;
 
 @Service
 public class BuyerService {
 	
 		@Autowired
 		BuyerRepository buyerRepository;
-		
-		@Autowired
-		PropertyRepository propertyRepository;
 	
-	    public String setAppointmentDate(long personId,LocalDate date) throws PersonNotFoundException{
+	    public String setAppointmentDate(long personId,LocalDate date) throws PersonNotFountException{
 	    	if(!buyerRepository.existsById(personId)) {
-				throw new PersonNotFoundException("Person not found!!");
-			}
-			else {
-				Buyer buyer = buyerRepository.findById(personId).get();
-				buyer.setAppointmentDate(date);
-				buyerRepository.save(buyer);
-				return "Appointment is set on "+date;
-			}
+	    		throw new PersonNotFountException("Person not found!!");
+	    	}
+	    	else {
+	    		Buyer buyer = buyerRepository.findById(personId).get();
+	    		buyer.setAppointmentDate(date);
+	    	
+	    		buyerRepository.save(buyer);
+	    		return "Appointment is set on "+date;
+	    		}
 	    }
 	
-	    public String getAppointmentDateAndTime(long personId) throws PersonNotFoundException{
-	    	if(!buyerRepository.existsById(personId)) {
-				throw new PersonNotFoundException("Person not found!!");
-			}
-			else {
-				Buyer buyer = buyerRepository.findById(personId).get();
+	    public String getAppointmentDateAndTime(long personId) throws PersonNotFountException{
+		    if(!buyerRepository.existsById(personId)) {
+		    	throw new PersonNotFountException("Person not found!!");
+		    }
+		    else {
+		    	Buyer buyer = buyerRepository.findById(personId).get();
 				LocalDate date = buyer.getAppointmentDate();
 				LocalTime time = buyer.getAppointmentTime();
 				return "Appointment for buyer with ID " + personId + " is on " + date +" "+time;  
-			}
+		    }
 	     }
 	
-		public Preference getPreferences(long personId) throws PersonNotFoundException{
-			if(!buyerRepository.existsById(personId)) {
-				throw new PersonNotFoundException("Person not found!!");
-			}
-			else {
-				Buyer buyer = buyerRepository.findById(personId).get();
-				Preference preferences = buyer.getPreferences();
-				return preferences;
-			}
+		public Preference getPreferences(long personId) throws PersonNotFountException{
+		    if(!buyerRepository.existsById(personId)) {
+		    	throw new PersonNotFountException("Person not found!!");
+		    }
+		    else {
+		    	Buyer buyer = buyerRepository.findById(personId).get();
+		    	Preference preferences = buyer.getPreferences();
+		    	return preferences;
+		    }
 		}
 	
 		
@@ -69,26 +64,26 @@ public class BuyerService {
 			
 		}
 		
-		public ResponseEntity<Object> deleteBuyer(long personId) throws PersonNotFoundException{
-			if(!buyerRepository.existsById(personId)) {
-				throw new PersonNotFoundException("Person not found!!");
-			}
-			else {
-				buyerRepository.deleteById(personId);
-				return new ResponseEntity<>("record deleted successfully",HttpStatus.OK);
-			}
+		public ResponseEntity<Object> deleteBuyer(long personId)throws PersonNotFountException{
+		    if(!buyerRepository.existsById(personId)) {
+		    	throw new PersonNotFountException("Person not found!!");
+		    }
+		    else {
+		    	buyerRepository.deleteById(personId);
+		    	return new ResponseEntity<>("record deleted successfully",HttpStatus.OK);
+		    }
 		}
 		
-		public List<Buyer> changePreferences(long personId, Preference preferences) throws PersonNotFoundException{
-			if(!buyerRepository.existsById(personId)) {
-				throw new PersonNotFoundException("Person not found!!");
-			}
-			else {
-				Buyer buyer = buyerRepository.findById(personId).get();
-				buyer.setPreferences(preferences);
-				buyerRepository.save(buyer);
-				return buyerRepository.findAll(); 
-			}
+		public List<Buyer> changePreferences(long personId, Preference preferences)throws PersonNotFountException{
+		    if(!buyerRepository.existsById(personId)) {
+		    	throw new PersonNotFountException("Person not found!!");
+		    }
+		    else {
+		    	Buyer buyer = buyerRepository.findById(personId).get();
+		    	buyer.setPreferences(preferences);
+		    	buyerRepository.save(buyer);
+		   		return buyerRepository.findAll(); 
+		    }
 		}
 		
 		public Buyer addBuyer(Buyer buyer)
@@ -96,29 +91,10 @@ public class BuyerService {
 			return buyerRepository.save(buyer);
 		}
 		
-		public boolean buyerExistsOrNot(long PersonId)
+		public boolean existOrNot(long id)
 		{
-			return buyerRepository.existsById(PersonId);
+			return buyerRepository.existsById(id);
 		}
 		
-		public ResponseEntity<Object> getBuyerById(long PersonId) throws PersonNotFoundException
-		{
-			if(!buyerRepository.existsById(PersonId))
-				throw new PersonNotFoundException("Person not found!!");
-			else
-			{
-				return new ResponseEntity<>(buyerRepository.findById(PersonId).get(),HttpStatus.OK);
-			}
-		}
 		
-		public List<Property> getPropertyByCity(String city) throws PropertyNotFoundException
-		{
-			List<Property> list=propertyRepository.findAll().stream().filter(f -> f.getAddress().getCity().
-					equals(city) && f.getSellOrRent().equalsIgnoreCase("sell")).collect(Collectors.toList());
-			if(list.isEmpty())
-				throw new PropertyNotFoundException("No such Property found at specified city for sale!!");
-			
-			return list;
-			
-		}
 }
